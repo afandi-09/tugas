1a39fc7b513f6bdfd479e3429282a97dfbe2c416
nandi <137107561+afandi-09@users.noreply.github.com>
2023-11-07 08:38:17 +0700

    Update adeh.js

---
+++ adeh.js
@@ -0,0 +1,4 @@
+//  contoh JavaScript DOM
+const name = document.getElemetById('namaKamu'); //ini klw mau menggunakan id
+const name = document.getElementsByClasName('namaKamu'); //ini klw mau pake clas
+//segera hadir yg lainya depeloper lapar mau makan dulu jan lupa makan 
